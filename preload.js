76669f09d7053cc4959130692cc6f8a7e4c144f4
Emrana Begum <emranabe@buffalo.edu>
2025-03-15 23:10:37 -0400

    Initialize React and Vite application with basic configuration and structure

---
+++ preload.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional for security
+      nodeIntegration: true, // Enable Node.js features in renderer
+    },
+  });
+
+  win.loadURL('http://localhost:5173'); // Change port if using Create React App
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
