0d4a2d8b6d336aa62c5316779d3e0fed18588fd5
Emrana Begum <emranabe@buffalo.edu>
2025-03-16 00:16:08 -0400

    Refactor application layout and add start page component with background image

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+/* createRoot is used to create a root to render your React application.
+This is part of the new React 18 API for concurrent renderring. */
 createRoot(document.getElementById('root')).render(
+  
+  /* StrictMode is a tool for highlighting potential problems in an application. 
+  It activates additional checks and warnings for its descendants. */
   <StrictMode>
     <App />
   </StrictMode>,
+  
 )
