33ae0cc2f09d611925e49c8d7c84cfd2fe70ce79
Emrana Begum <emranabe@buffalo.edu>
2025-05-18 19:37:43 -0400

    Enhance navigation layout and styling; add home icon with updated CSS for improved responsiveness and visual appeal.

--- my-app/src/component/background.jsx
+++ my-app/src/component/background.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import '../css/background.css';
 import logo from '../assets/logo.png';
-import { Camera } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 
 const Background = ({ children }) => {
 	return (
 		<div className="background">
 			<div className='top-nav'>
-				<div className='left-nav'><img src={Camera}></img></div>
-				<div className='mid-nav'><img src={logo} className="nav-logo" alt="Logo" /><h4>SmartChoice</h4></div>
-				<div className='right-nav'><h4>x</h4></div>
+				<div className='left-nav'>
+					<Menu className='nav-icon close-icon'/>
+				</div>
+				<div className='mid-nav'>
+					<div className='nav-icon home-icon'>
+						<img src={logo} className="nav-logo" alt="Logo" />
+						<p className='home-icon'>SmartChoice</p>
+					</div>
+				</div>
+				<div className='right-nav'>
+					<X className='nav-icon close-icon'/>
+				</div>
 
 			</div>
 			<div className='content'>
