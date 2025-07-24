953eb9cffca2104e5d6d2546d241c8966e2ba12d
Emrana Begum <emranabe@buffalo.edu>
2025-07-24 19:58:28 -0400

    Fix import path for CategoryPage CSS and add new background CSS file; update MoodSelector icon dimensions for better visibility

--- my-app/src/component/CategoryPage.jsx
+++ my-app/src/component/CategoryPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import "../css/CategoryPage"
+import "../css/CategoryPage.css"
 
 const Category = () => {
 	const { name } = useParams();
